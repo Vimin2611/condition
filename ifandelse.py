8a5b7625102691b60d6cbbed23b74ee5bce2c08a
Vimin2611 <krish8102@gmail.com>
2025-03-13 18:02:43 +0000

    Add files via upload

---
+++ ifandelse.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the number of your choice :"))
+if (num > 15) :
+    print ("Your number is more than 15")
+    print("I am in if block")
+
+else :
+    print("Your number is less than 15")
+    print("I am in else block")
+
+print(" I am not in if or else block")
